890a1ac0741f0f5b16d15d43e897a0223865970a
Ekunola John Oluwatosin <ekunolajohn@gmail.com>
2023-12-27 15:20:07 +0100

    new project added

--- components/Project.jsx
+++ components/Project.jsx
@@ -8,6 +8,8 @@ import jayImg from "../public/assets/image/projects/jayStore.jpg";
 import amazon from "../public/assets/image/projects/amazon.png";
 import Dash from "../public/assets/image/projects/dash.jpg";
 import Movix from "../public/assets/image/projects/movix.jpg";
+import tiktok from "../public/assets/image/projects/tiktok.jpg";
+import gallery from "../public/assets/image/projects/gallery.jpg";
 
 const Projects = () => {
   return (
@@ -19,9 +21,21 @@ const Projects = () => {
         <h2 className="py-4">What I&apos;ve Built</h2>
         <div className="grid md:grid-cols-2 gap-8">
           <ProjectItem
+            title="Tiktok-Clone"
+            backgroundImg={tiktok}
+            projectUrl="/Tiktok"
+            tech="NextJs"
+          />
+          <ProjectItem
             title="Amazon-Clone"
             backgroundImg={amazon}
             projectUrl="/Amazon"
+            tech="NextJs"
+          />
+          <ProjectItem
+            title="Gallery"
+            backgroundImg={gallery}
+            projectUrl="/Gallery"
             tech="NextJs"
           />
           <ProjectItem
