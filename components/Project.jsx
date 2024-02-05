5b1abb5c1fde04bda34612b187e036c21f91c8c9
Ekunola John Oluwatosin <ekunolajohn@gmail.com>
2024-02-05 12:49:01 +0100

    thread app added

--- components/Project.jsx
+++ components/Project.jsx
@@ -8,7 +8,8 @@ import jayImg from "../public/assets/image/projects/jayStore.jpg";
 import amazon from "../public/assets/image/projects/amazon.png";
 import Dash from "../public/assets/image/projects/dash.jpg";
 import Movix from "../public/assets/image/projects/movix.jpg";
-import tiktok from "../public/assets/image/projects/tiktok.jpg";
+// import tiktok from "../public/assets/image/projects/tiktok.jpg";
+import threads from "../public/assets/image/projects/threads.jpg";
 import gallery from "../public/assets/image/projects/gallery.jpg";
 
 const Projects = () => {
@@ -20,10 +21,16 @@ const Projects = () => {
         </p>
         <h2 className="py-4">What I&apos;ve Built</h2>
         <div className="grid md:grid-cols-2 gap-8">
-          <ProjectItem
+          {/* <ProjectItem
             title="Tiktok-Clone"
             backgroundImg={tiktok}
             projectUrl="/Tiktok"
+            tech="NextJs"
+          /> */}
+          <ProjectItem
+            title="Threads-Clone"
+            backgroundImg={threads}
+            projectUrl="/Threads"
             tech="NextJs"
           />
           <ProjectItem
